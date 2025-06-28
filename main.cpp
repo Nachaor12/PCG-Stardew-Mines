f57948bc9ac2506c379c615fd332d1e77699bc7f
iondrw <f.f.ortega40@gmail.com>
2025-06-28 17:49:52 -0400

    GenerarRuido Funcion

--- main.cpp
+++ main.cpp
@@ -21,20 +21,25 @@ void printMap(const Map& map) {
     std::cout << "-------------------" << std::endl;
 }
 
+Map GenerarRuido(const Map& currentMap, int W, int H){
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    srand(seed);
+    Map noisyMap = currentMap;
+     for (int y = 0; y < H; ++y) {
+        for (int x = 0; x < W; ++x) {
+            noisyMap[x][y] = ((rand() % 30) < 2) ? 1 : 0;
+        }
+    }
+    return noisyMap;
+}
+
 //cellular automata con modificacion
 //a este se le agregan limites de las paredes, que el tamaño también varie a partir de un rango
 //Tambien se le agrega un modificador para que no queden espacios intransitables
 Map cellularAutomata(const Map& currentMap, int W, int H, int R, double U) {
-    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
-    srand(seed);
     Map newMap = currentMap; // Initially, the new map is a copy of the current one
+
    
-    //se genera el ruido
-    for (int y = 0; y < H; ++y) {
-        for (int x = 0; x < W; ++x) {
-            newMap[x][y] = ((rand() % 30) < 2) ? 1 : 0;
-        }
-    }
     Map noisyMap = newMap;
     //se recorre el mapa nuevo
     for (int y = 0; y < H ; y++){
@@ -87,6 +92,7 @@ int main(){
     // --- Main Simulation Loop ---
     for (int iteration = 0; iteration < 1; ++iteration) {
         //std::cout << "\n--- Iteration " << iteration + 1 << " ---" << std::endl;
+        myMap = GenerarRuido(myMap, mapRows, mapCols);
         myMap = cellularAutomata(myMap, mapRows, mapCols, radiusNoise, limitNoise);
         printMap(myMap);
     }
