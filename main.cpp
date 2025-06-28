a23244491f4b4d6fa8845fa325d432bb49429de8
Nachaor12 <163567793+Nachaor12@users.noreply.github.com>
2025-06-28 00:21:59 -0400

    cpp main para el proyecto

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//Código para la generación procedural de minas para Stardew Valley basado en Cellular automata con Room templates y sonido.
+//Félix Fuentes e Ignacio Alfaro 
+
+
+
+int main(){
+    return 0;
+}
